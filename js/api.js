cc7f29363ad9ea245987ba16ae04ca18e5f5189e
Ruthskery <cyrilimperial12@gmail.com>
2025-12-27 11:12:41 +0800

    API Fixed

--- js/api.js
+++ js/api.js
@@ -1,5 +1,11 @@
+const API_BASE =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+    ? "http://localhost:3000"
+    : "https://gtkmc-maze-server.onrender.com";
+
 export function sendMazeResult(data) {
-  fetch("https://gtkmc-maze-server.onrender.com/api/maze-result", {
+  fetch(`${API_BASE}/api/maze-result`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data)
